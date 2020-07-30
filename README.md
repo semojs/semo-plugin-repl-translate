8a6d3a79683da5782709c5eb59aaee9304ff59fb
Richard Yu <vipzhicheng@gmail.com>
2020-07-30 11:08:02 +0800

    init: init semo-plugin-repl-translate

---
+++ README.md
@@ -0,0 +1,19 @@
+# semo-plugin-repl-translate
+
+A Semo plugin to provide ability to translate in REPL
+
+## Installation
+
+```
+npm i -g @semo/cli semo-plugin-repl-translate
+semo -r --hook
+>>> .translate hello
+```
+
+## Thanks
+
+This semo plugin use `fanyi` npm package to fetch translated output.
+
+## License
+
+MIT
---
+++ src/hooks/index.ts
@@ -0,0 +1,26 @@
+export = (Utils) => {
+  return {
+    hook_repl_command: new Utils.Hook('semo', () => {
+      return {
+        translate: {
+          help: 'Translate words and sentense.',
+          async action(input) {
+            if (!input) {
+              Utils.warn('keyword is required')
+            } else {
+              let parseInput = Utils.yParser(input)
+              let words = parseInput._.join(' ')
+              // @ts-ignore
+              this.clearBufferedCommand()
+
+              Utils.exec(`npx fanyi ${words}`)
+             
+              // @ts-ignore
+              this.displayPrompt()
+            }
+          }
+        }
+      }
+    })
+  }
+}
